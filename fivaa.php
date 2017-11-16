18f6a8749d84649e845de28a077f9218a3dc83cf
jrpikong <jr.pikong@gmail.com>
2017-11-17 06:48:34 +0700

    add fivaa test case no 1

---
+++ fivaa.php
@@ -0,0 +1,10 @@
+<?php
+        for ($i=4;$i>=0;$i--){
+            echo $i.$i;
+            $n=$i + 2;
+            for ($j=$i+1;$j>0;$j--) {
+                    echo $n;
+            }
+            echo "<br>";
+        }
+  
